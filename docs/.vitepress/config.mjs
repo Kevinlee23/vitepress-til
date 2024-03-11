2e17fc7f08f87354c848ee7cffcf7119636add27
luxiaxue <15573097235@163.com>
2024-03-11 21:54:27 +0800

    fix: 改为默认的部署地址

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/MyTIL',
   title: "Vitepress-TIL",
   description: "A VitePress Site",
   themeConfig: {
