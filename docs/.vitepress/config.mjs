87fa1cc18a2879aaa73ed5cfde3a6cc295c0fdbc
luxiaxue <15573097235@163.com>
2024-03-11 22:04:47 +0800

    fix: base 地址修改为项目地址

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitepress-til/',
   title: "Vitepress-TIL",
   description: "A VitePress Site",
   themeConfig: {
