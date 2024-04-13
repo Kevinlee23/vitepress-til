f55e1e6bcb00731e6f24594aef8b92b63ed8137c
snowinlu <15573097235@163.com>
2024-04-13 13:16:31 +0800

    feat: 增加gotop组件

---
+++ docs/.vitepress/theme/components/backTop.vue
@@ -0,0 +1,43 @@
+<template>
+  <Transition>
+    <div
+      v-if="y !== 0"
+      class="go-top-button"
+      style="position: fixed; right: 40px; bottom: 40px"
+      @click="handleToTop"
+    >
+      Go Top 👆
+    </div>
+  </Transition>
+</template>
+
+<script lang="ts" setup>
+import { useWindowScroll } from "@vueuse/core";
+
+const { y } = useWindowScroll();
+
+const handleToTop = () => {
+  y.value = 0;
+};
+</script>
+
+<style lang="scss">
+.go-top-button {
+  padding: 12px;
+  border-radius: 8px;
+  cursor: pointer;
+
+  color: white;
+  background-color: #2c9678;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import asideTop from "./components/asideTop.vue"
+import backTop from './components/backTop.vue'
 import './fira_code.css'
 
 export default {
@@ -8,6 +9,7 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       "aside-top": () => h(asideTop),
+      "doc-footer-before": () => h(backTop)
     })
   }
 }
