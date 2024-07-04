61029b486f66af63e65a9b044d9500ac85d70d80
snowinlu <15573097235@163.com>
2024-07-04 15:40:39 +0800

    feat: 样式调整

--- docs/.vitepress/theme/components/tagGroup.vue
+++ docs/.vitepress/theme/components/tagGroup.vue
@@ -12,9 +12,11 @@ const hostpath = "/vitepress-til";
 <template>
   <div v-for="group in tagGroups" class="tag-group">
     <template v-for="(item, index) in group.items">
-      <a class="tag-item" :href="`${hostpath}${item.link}`">
-        {{ item.title }}
-      </a>
+      <span class="tag-item">
+        <a :href="`${hostpath}${item.link}`">
+          {{ item.title }}
+        </a>
+      </span>
       <Badge
         v-if="index === group.items.length - 1"
         type="tip"
@@ -28,7 +30,27 @@ const hostpath = "/vitepress-til";
 .tag-group {
   padding: 8px 0;
   .tag-item {
-    padding-right: 12px;
+    @apply relative mr-[12px];
+
+    &:before {
+      content: "";
+      @apply w-full absolute left-auto right-0 top-auto bottom-0 border-b-[1px] border-b-[rgba(100,108,255,0.86)];
+      transition: all 336ms cubic-bezier(0.48, 0.04, 0.52, 0.96);
+
+      &:hover {
+        @apply w-0;
+      }
+    }
+
+    &:after {
+      content: "";
+      @apply w-0 absolute left-0 top-auto bottom-0 border-b-[1px] border-b-[#2c9678];
+      transition: all 336ms cubic-bezier(0.48, 0.04, 0.52, 0.96) 200ms;
+
+      &:hover {
+        @apply w-full;
+      }
+    }
   }
 
   a {
