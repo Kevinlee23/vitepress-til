4327d0f2d67962f52c9391aeafba174e5a950dff
snowinlu <15573097235@163.com>
2024-10-10 15:30:55 +0800

    feat: 测试rss

---
+++ docs/.vitepress/utils/rss.ts
@@ -0,0 +1,106 @@
+import { dirname } from "path";
+import fg from "fast-glob";
+import fs from "fs-extra";
+import matter from "gray-matter";
+import MarkdownIt from "markdown-it";
+import type { FeedOptions, Item } from "feed";
+import { Feed } from "feed";
+
+const DOMAIN = "https://kevinlee23.github.io";
+const AUTHOR = {
+  name: "Snowinlu",
+  email: "snowinlu@gmail.com",
+  link: DOMAIN + "/vitepress-til/",
+};
+const OPTIONS: FeedOptions = {
+  title: "Snowinlu",
+  description: "Snowinlu'blog",
+  id: `${DOMAIN}/`,
+  link: `${DOMAIN}/vitepress-til/`,
+  copyright: "MIT License",
+  feedLinks: {
+    json: `${DOMAIN}/vitepress-til` + "/feed.json",
+    atom: `${DOMAIN}/vitepress-til` + "/feed.atom",
+    rss: `${DOMAIN}/vitepress-til` + "/feed.xml",
+  },
+  author: AUTHOR,
+  image: `${DOMAIN}/vitepress-til/cola.svg`,
+  favicon: `${DOMAIN}/vitepress-til/cola.svg`,
+};
+
+const markdown = MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+});
+
+export async function buildBlogRSS() {
+  const posts = await generateRSS();
+  writeFeed("feed", posts);
+}
+
+async function generateRSS() {
+  const folders = [
+    "javascript",
+    "typescript",
+    "css",
+    "vue3",
+    "react",
+    "react-native",
+    "nest",
+    "nuxt",
+    "next",
+    "config",
+    "fix",
+    "mark",
+    "ordinary",
+    "http",
+    "git",
+    "sql",
+    "mongodb",
+    "oss",
+    "docker",
+    "3-party-library",
+    "ops",
+    "pmp",
+    "echarts",
+  ];
+
+  const files = await fg(folders.map((item) => item + "/*.md"));
+
+  const posts: any[] = (
+    await Promise.all(
+      files
+        .filter((i) => !i.includes("index"))
+        .map(async (i) => {
+          const raw = await fs.readFile(i, "utf-8");
+          const { data, content } = matter(raw);
+          const html = markdown
+            .render(content)
+            .replace('src="/', `src="${DOMAIN}/vitepress-til/`);
+
+          return {
+            ...data,
+            date: new Date(data.date),
+            content: html,
+            author: [AUTHOR],
+            link: `${DOMAIN}/vitepress-til/${i.replace(".md", ".html")}`,
+          };
+        })
+    )
+  ).filter(Boolean);
+
+  posts.filter((item) => item.date);
+  posts.sort((a, b) => +new Date(b.date) - +new Date(a.date));
+  return posts;
+}
+
+async function writeFeed(name: string, items: Item[]) {
+  const feed = new Feed(OPTIONS);
+  items.forEach((item) => feed.addItem(item));
+
+  await fs.ensureDir(dirname(`./.vitepress/dist/${name}`));
+  await fs.writeFile(`./.vitepress/dist/${name}.xml`, feed.rss2(), "utf-8");
+  await fs.writeFile(`./.vitepress/dist/${name}.atom`, feed.atom1(), "utf-8");
+  await fs.writeFile(`./.vitepress/dist/${name}.json`, feed.json1(), "utf-8");
+}
---
+++ docs/css/motion-definition.md
@@ -0,0 +1,31 @@
+---
+title: Css 动画定义
+date: 2024-10-10
+---
+
+# Css 动画定义
+
+## 动画样式
+
+- `Fade` 渐入/渐出
+- `Slide` 滑入/滑出
+- `Focus` 聚焦
+- `Blur` 模糊
+- `Bounce` 反弹
+- `Expand` 扩展
+- `Shrink` 收缩
+- `Pop` 弹出
+- `Compress` 压缩
+- `Shake` 摇摆
+- `Confetti` 彩带
+- `Typewriter` 打字机模式
+
+## 动画附加条件
+
+- `delay` 延时
+- `duration` 时长
+- `translate-x, translate-y` 平移
+- `scale` 尺寸
+- `rotate` 旋转
+- `in` 进入动画
+- `out` 离开动画
