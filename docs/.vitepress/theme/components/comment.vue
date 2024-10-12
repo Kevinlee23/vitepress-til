c51cbe1c77238d57b377253300d058a3fb5629a0
snowinlu <15573097235@163.com>
2024-10-12 11:30:55 +0800

    feat: 新增 gitalk 评论组件

---
+++ docs/.vitepress/theme/components/comment.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="showFlag" id="comment-container"></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref, nextTick } from "vue";
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+import md5 from "blueimp-md5";
+
+const showFlag = ref(true);
+const gitDefault = {
+  clientID: "Ov23li60QSciq9KaehKQ",
+  clientSecret: "1ff63abbe933dc252b24df9982e9de001391aa96",
+  repo: "vitepress-til",
+  owner: "Kevinlee23",
+  admin: ["Kevinlee23"],
+  id: md5(location.pathname),
+  language: "zh-CN",
+};
+
+onMounted(() => {
+  nextTick(() => {
+    const gitalk = new Gitalk(gitDefault);
+    gitalk.render("comment-container");
+  });
+});
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,12 +3,13 @@ import DefaultTheme from "vitepress/theme-without-fonts";
 import asideTop from "./components/asideTop.vue";
 import backTop from "./components/backTop.vue";
 import copyright from "./components/copyright.vue";
+import comment from "./components/comment.vue";
 import "./lxgw.font.css";
 import "./fira_code.css";
 import "./tailwind.postcss.css";
 import "virtual:group-icons.css";
 import "./my-fonts.css";
-import "./custom.css"
+import "./custom.css";
 
 export default {
   ...DefaultTheme,
@@ -17,6 +18,7 @@ export default {
       "aside-top": () => h(asideTop),
       "aside-outline-after": () => h(backTop),
       "layout-bottom": () => h(copyright),
+      "doc-after": () => h(comment),
     });
   },
 };
