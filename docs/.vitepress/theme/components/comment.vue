9e8aca3b650be8dced568e15efa6d709f96ab851
snowinlu <15573097235@163.com>
2024-10-16 09:42:41 +0800

    fix: 页面跳转时，同步更新 comment

--- docs/.vitepress/theme/components/comment.vue
+++ docs/.vitepress/theme/components/comment.vue
@@ -3,25 +3,40 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, nextTick } from "vue";
+import { ref } from "vue";
+import { onContentUpdated } from "vitepress";
 import "gitalk/dist/gitalk.css";
 import Gitalk from "gitalk";
 
-const showFlag = ref(true);
-const gitDefault = {
-  clientID: "Ov23li60QSciq9KaehKQ",
-  clientSecret: "1ff63abbe933dc252b24df9982e9de001391aa96",
-  repo: "vitepress-til",
-  owner: "Kevinlee23",
-  admin: ["Kevinlee23"],
-  id: location.pathname.substring(0, 50),
-  language: "zh-CN",
-};
+function deleteChild(element: HTMLDivElement | null) {
+  let child = element?.lastElementChild;
+  while (child) {
+    element?.removeChild(child);
+    child = element?.lastElementChild;
+  }
+}
 
-onMounted(() => {
-  nextTick(() => {
-    const gitalk = new Gitalk(gitDefault);
-    gitalk.render("comment-container");
-  });
+const showFlag = ref(true);
+onContentUpdated(() => {
+  const gitDefault = {
+    clientID: "Ov23li60QSciq9KaehKQ",
+    clientSecret: "1ff63abbe933dc252b24df9982e9de001391aa96",
+    repo: "vitepress-til",
+    owner: "Kevinlee23",
+    admin: ["Kevinlee23"],
+    id: location.pathname.substring(0, 50),
+    language: "zh-CN",
+  };
+
+  const element: HTMLDivElement | null =
+    document.querySelector("#comment-container");
+  if (!element) {
+    return;
+  }
+
+  deleteChild(element);
+
+  const gitalk = new Gitalk(gitDefault);
+  gitalk.render("#comment-container");
 });
 </script>
