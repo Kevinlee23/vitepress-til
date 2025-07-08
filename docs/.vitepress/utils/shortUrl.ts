7f8a142286893649153443ec7751c455cb9af455
lixiuxian1997 <15573097235@163.com>
2025-07-08 14:00:24 +0800

    feat: rss 时间错误

--- docs/.vitepress/utils/shortUrl.ts
+++ docs/.vitepress/utils/shortUrl.ts
@@ -2,33 +2,36 @@ import md5 from "blueimp-md5";
 import { MY_TAB, type IItems, createSidebar } from "./createSideBar";
 
 export interface IShortUrlMap {
-	[key: string]: string;
+  [key: string]: string;
 }
 
-export function createShortUrlMap() {
-	const allSideBar = createSidebar();
-	const tabs = Object.keys(allSideBar) as MY_TAB[];
-	const long2short: IShortUrlMap = {};
-	const short2long: IShortUrlMap = {};
-	function findItems(items: IItems[]) {
-		for (const item of items) {
-			if (item["items"]) {
-				findItems(item["items"]);
-			} else {
-				const link = item.link;
+export function createShortUrlMap(): [IShortUrlMap, IShortUrlMap] {
+  const allSideBar = createSidebar();
+  const tabs = Object.keys(allSideBar) as MY_TAB[];
+  const long2short: IShortUrlMap = {};
+  const short2long: IShortUrlMap = {};
+  function findItems(items?: IItems[]) {
+		if(!items) return "";
+
+    for (const item of items) {
+      if (item["items"]) {
+        findItems(item["items"]);
+      } else {
+        const link = item.link;
         if (link) {
           const shortUrl = md5(link).slice(0, 11);
           long2short[link] = shortUrl;
           short2long[shortUrl] = link;
         }
-			}
-		}
-	}
-	tabs.forEach((tab) => {
-		findItems(allSideBar[tab]);
-	});
+      }
+    }
+  }
 
-	return [long2short, short2long];
+  tabs.forEach((tab) => {
+    findItems(allSideBar[tab]);
+  });
+
+  return [long2short, short2long];
 }
 
 export const [long2short, short2long] = createShortUrlMap();
